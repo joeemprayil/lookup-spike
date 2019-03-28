5357bae46e9c209a8e6b100d95b504a2053970a2
Joe Emprayil <joe.emprayil@imanage.com>
2019-03-28 10:21:13 -0500

    switched to single list instead of multiple list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AutoCompleteModule} from 'primeng/autocomplete';
-import {PickListModule} from 'primeng/picklist';
+import {OrderListModule} from 'primeng/orderlist';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
@@ -24,7 +24,7 @@ import { PickListComponent } from './pick-list/pick-list.component';
     FormsModule,
     AutoCompleteModule,
     DragDropModule,
-    PickListModule
+    OrderListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
